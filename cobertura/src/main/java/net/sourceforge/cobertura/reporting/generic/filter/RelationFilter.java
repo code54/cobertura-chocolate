3fa06f1b857671cc5dfcb250d840a58d580f4853
Jose Martin Rozanec <jose@code54.com>
2012-10-16 14:34:46 -0300

    Add licence to files

--- cobertura/src/main/java/net/sourceforge/cobertura/reporting/generic/filter/RelationFilter.java
+++ cobertura/src/main/java/net/sourceforge/cobertura/reporting/generic/filter/RelationFilter.java
@@ -7,6 +7,26 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+/*
+ * Cobertura - http://cobertura.sourceforge.net/
+ *
+ * Copyright (C) 2012 Jose M. Rozanec
+ *
+ * Cobertura is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * Cobertura is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cobertura; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
 public class RelationFilter implements Filter{
 
     private Criteria criteria;
--- cobertura/src/main/java/net/sourceforge/cobertura/reporting/generic/filter/criteria/ANDListedCriteria.java
+++ cobertura/src/main/java/net/sourceforge/cobertura/reporting/generic/filter/criteria/ANDListedCriteria.java
@@ -2,6 +2,26 @@ package net.sourceforge.cobertura.reporting.generic.filter.criteria;
 
 import java.util.Set;
 
+/*
+ * Cobertura - http://cobertura.sourceforge.net/
+ *
+ * Copyright (C) 2012 Jose M. Rozanec
+ *
+ * Cobertura is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * Cobertura is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cobertura; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
 public class ANDListedCriteria implements Criteria {
 
     private Set<Criteria> criterias;
